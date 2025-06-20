4261e8fe449d9d711e24c0cf52f9a7ae765f9b6d
Paul <paul.overbeck95@gmail.com>
2025-06-20 16:24:00 +0200

    ex02 done

---
+++ CPP02/ex02/Fixed.cpp
@@ -0,0 +1,234 @@
+
+
+#include "Fixed.hpp"
+
+
+Fixed::Fixed(){
+     std::cout << "Default constructor called" << std::endl;
+     this->fixed_point_Nbr= 0; 
+}; 
+
+// convert to fixed-point values -> init to 8;
+//  Shift bits left by 8 to convert to fixed point
+//  '<<' now as bitshift operator '8' positions 
+// 2 << 8 = 2*256 = 512 ( 2 ^ fraction_bit)
+Fixed::Fixed(const int value)
+{
+    std::cout << "Int constructor called" << std::endl;
+    this->fixed_point_Nbr = value << this->fraction_bit;
+};
+
+//converts float to fixed-point value(int)
+// dezimalstellen werden gerundet
+// 1 << fraction_bit= (0000 0001) = 256 -> 0001 0000 0000
+// saves->  1,5 *256 = 384 (binary) 0000 0001 1000 0000
+// convertig back 384/256 = 1,5 in toFloat
+Fixed::Fixed(const float float_value)
+{
+    std::cout << "Float constructor called" << std::endl;
+    this->fixed_point_Nbr = roundf(float_value *(1 << this->fraction_bit));
+};
+
+Fixed::Fixed(const Fixed& other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    this->fixed_point_Nbr = other.fixed_point_Nbr;
+
+};
+
+Fixed::~Fixed(){
+    std::cout << "Destructor called" << std::endl;
+};
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;\
+    if(this != &other)
+    {
+        this->fixed_point_Nbr = other.fixed_point_Nbr;
+        // this->fixed_point_Nbr = other.getRawBits();
+    }
+    return *this;
+};
+
+int Fixed::getRawBits(void) const
+{
+    return(this->fixed_point_Nbr);
+};
+
+ void Fixed::setRawBits (int const raw)
+ {
+    this->fixed_point_Nbr = raw;
+ };
+
+
+
+// 384 becomes 384.0 (float)
+//  shifted left bei 8 zeros -> 1 becomes 256 (Fraction part is 8 bits long)
+// smaller Fraction part -> more space for integer part (32 bits so 32 0 and 1)
+// 1 << this->fraction_bit so its scaleable 2^fraction_bit
+// convertig back 384/256 = 1,5 
+float Fixed::toFloat( void ) const{
+    float value = (float)this->fixed_point_Nbr; 
+    float dominator = (1 << this->fraction_bit);
+    return value/dominator;
+};
+
+
+// fixed point nbr 
+int Fixed::toInt(void) const {
+    // std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
+    int value = this->fixed_point_Nbr;
+    return value >> this->fraction_bit;
+};
+
+
+
+// friend function
+std::ostream& operator<<(std::ostream& output_obj, const Fixed&object_fixed)
+{
+    output_obj << object_fixed.toFloat();
+    return(output_obj);
+}
+
+
+
+bool Fixed::operator<(const Fixed& out_object) const
+{
+    if(this->fixed_point_Nbr < out_object.fixed_point_Nbr)
+        return true;
+    return false;
+};
+
+bool Fixed::operator>(const Fixed& out_object) const
+{
+    if(this->fixed_point_Nbr > out_object.fixed_point_Nbr)
+        return true;
+    return false;
+};
+
+bool Fixed::operator<=(const Fixed& out_object) const
+{
+    if(this->fixed_point_Nbr <= out_object.fixed_point_Nbr)
+        return true;
+    return false;
+};
+
+bool Fixed::operator>=(const Fixed& out_object) const
+{
+    if(this->fixed_point_Nbr >= out_object.fixed_point_Nbr)
+        return true;
+    return false;
+};
+
+bool Fixed::operator==(const Fixed& o_object) const
+{
+    if(this->fixed_point_Nbr == o_object.fixed_point_Nbr)
+        return true;
+    return false;
+}
+
+bool Fixed::operator!=(const Fixed& o_object) const
+{
+    if(this->fixed_point_Nbr != o_object.fixed_point_Nbr)
+        return true;
+    return false;
+}
+
+
+// Arithmetic operators
+// return by value not by reference (Fixed&)
+Fixed Fixed::operator*(const Fixed& o_object) const
+{
+    Fixed tmp_object;
+    tmp_object.fixed_point_Nbr = (long long)this->fixed_point_Nbr *o_object.fixed_point_Nbr;// save for overflow
+    tmp_object.fixed_point_Nbr = tmp_object.fixed_point_Nbr >> fraction_bit;
+    return (tmp_object);
+}
+
+/*
+a = 1,5 -> fixed_point_Nbr = round (1,5 *256) = 384 in binary 0000 0001 1000 0000 (8 bits reserved for the fraction part)
+b = 2    -> fixed_point_Nbr = round (2 * 256) = 512 in binary 0000 0011 0000 0000 
+
+384 * 512 = 196608 
+in binary => 0000 0011 0000 0000 0000 0000 0000
+both nubers are scaled by 256 -> result is scaled by 256*256
+-> shift back by 8 bits(Franction part) divide by 256
+
+*/
+
+Fixed Fixed::operator/(const Fixed& o_object) const
+{
+    Fixed tmp_object;
+    long long numerator = (long long)this->fixed_point_Nbr << fraction_bit;// get fraction part shift 8 binary to the left ( *256)
+    tmp_object.fixed_point_Nbr = numerator  / o_object.fixed_point_Nbr; 
+    return (tmp_object);
+}
+/*
+Fixed(5.05f) -> fixed_point_Nbr = round(5.05 *256) = 1293
+Fixed(2)      -> fixed_point_Nbr = 2 << 8 = 512
+BUT 523 / 256 = 2.0 it should be 2.525
+1293 / 512 = 2      int division, fractional part lost 
+*/
+
+Fixed Fixed::operator+(const Fixed& o_object) const
+{
+    Fixed tmp_object;
+    long long a = (long long) this->fixed_point_Nbr << fraction_bit;
+    long long b = (long long) o_object.fixed_point_Nbr << fraction_bit;
+    tmp_object.fixed_point_Nbr = (a + b) >> fraction_bit;
+    return(tmp_object);
+}
+
+Fixed Fixed::operator-(const Fixed& o_object) const 
+{
+    Fixed tmp_object;
+    long long a = (long long) this->fixed_point_Nbr << fraction_bit;
+    long long b = (long long) o_object.fixed_point_Nbr << fraction_bit;
+    tmp_object.fixed_point_Nbr = (a - b) >> fraction_bit;
+    return(tmp_object);
+}
+
+
+// increment, decrement  
+
+// pre increment 
+// ++a ->       a.operator++();
+// ++(++a) works by returning by reference 
+// if returnung by value-> returns a temporary , can not increment it temporary 
+Fixed& Fixed::operator++()
+{
+    this->fixed_point_Nbr += 1;
+    return (*this);
+}
+// post increment 
+// a++ -> a.operator++(0) // return a copy before incrementing copy is temporay
+// int as a dummy parameter to distinguish pre and post increm (int = 0);
+Fixed Fixed::operator++(int) 
+{
+    Fixed copy_obj;
+    // copy_obj = this->fixed_point_Nbr; use of int constructor 
+    copy_obj  = *this; // only parse a pointer, calls copy constructor
+    this->fixed_point_Nbr += 1;
+    return(copy_obj);
+}
+
+// compilor differs automatecially if given parameter is const object, a const object needs to bee returned
+Fixed& Fixed::min( Fixed& a_object, Fixed& b_object)
+{
+    return(a_object < b_object) ? a_object : b_object;
+}
+
+Fixed& Fixed::max(Fixed& a , Fixed& b)
+{
+    return(a > b) ? a : b;
+}
+
+const Fixed& Fixed::min( const Fixed& a_object, const Fixed& b_object)
+{
+    return(a_object < b_object) ? a_object : b_object;
+}
+const Fixed& Fixed::max( const Fixed& a , const Fixed& b )
+{
+    return(a > b) ? a : b;
+}
---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,70 @@
+
+#include "Fixed.hpp"
+#include <cmath> 
+#include <iostream>
+
+int main( void ) 
+{
+    Fixed a;
+    Fixed const b( 10 );
+    Fixed const c( 42.42f );
+    // Fixed const d( b );
+    // a = Fixed( 1234.4321f );
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    // std::cout << "d is " << d << std::endl;
+    // std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    // std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    // std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    // std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    if (b < c) // a.operator<(c);
+        std::cout << b << " b is smaler then c " << c << std::endl;
+
+        
+    // Fixed a;
+
+
+    Fixed const d( Fixed( 5.05f ) * Fixed( 2 ) ); // Fixed(5.05f).operator*(Fixed(2))
+    Fixed const f( Fixed( 5.05f ) / Fixed( 2 ) );// std::cout << a << std::endl;
+    Fixed g( Fixed( 5.05f ) + Fixed( 2 ) );// std::cout << a << std::endl;
+    Fixed h( Fixed( 5.05f ) - Fixed( 2 ) );// std::cout << a << std::endl;
+    // std::cout << ++a << std::endl;
+    // std::cout << f << std::endl;
+    // std::cout << a++ << std::endl;
+    std::cout << "result of multiplication  5.05f with 2:  " << d << std::endl;
+    std::cout << "result of division        5.05f with 2:  " << f << std::endl;
+    std::cout << "result of addition        5.05f with 2:  " << g << std::endl;
+    std::cout << "result of substraction    5.05f with 2:  " << h << std::endl;
+
+    std::cout << "minimum pre- increment of h (3.305075):  " << ++h << std::endl;
+    std::cout << "value of h after incr   h (3.305075):    " << h << std::endl;
+    std::cout << "minimum post- increment of h (3.305075): " << h++ << std::endl; // temporary created by copy assignment 
+    std::cout << "value of h after incr   h (3.305075):    " << h << std::endl;
+    // std::cout << Fixed::max( a, b ) << std::endl;
+    
+    Fixed& x = Fixed::max( h, g );
+    std::cout << "max of d:   "<< d <<  "and f: "<< f << " max: "<< Fixed::max( d, f ) << std::endl;
+    std::cout << "max of h:   "<< h <<  "and g: "<< g << " max: "<< x << std::endl;
+
+    Fixed& y = Fixed::max( h, g );
+    std::cout << "min of d:   "<< d <<  "and f: "<< f << " min: "<< Fixed::min( d, f ) << std::endl;
+    std::cout << "min of h:   "<< h <<  "and g: "<< g << " min: "<< y << std::endl;
+
+
+    // Fixed a;
+    // Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    // std::cout << a << std::endl;
+    // std::cout << ++a << std::endl;
+    // std::cout << a << std::endl;
+    // std::cout << a++ << std::endl;
+    // std::cout << a << std::endl;
+    // std::cout << b << std::endl;
+    // // std::cout << Fixed::max( a, b ) << std::endl;
+    // return 0;
+
+
+    return 0;
+}
+
